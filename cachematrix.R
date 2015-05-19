8cc747d5bd1565ece558c0593ca339fe38aff479
Tim C <tcody1@yahoo.com>
2015-05-19 14:35:12 -0400

    commit of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## the makeCacheMatrix function will generate a matrix object and then cache the inverser
+## the cacheSolve function will return the inverse of the cached matrix returned by makeCacheMatrix
+## 
+## need to add comments
 makeCacheMatrix <- function(x = matrix()) {
-
+i <- NULL
+    set <- function(y) {
+      x <<- y
+      i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) i <<- solve
+    getinv <- function() i
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## need to add comments 
+cacheSolve <- function(z, ...) {
+        ## This will return a matrix that is the inverse of 'z'
+		i<- z$getinv()
+    if(!is.null(i)) {
+      message("obtaining data")
+      return(i)
+    }
+    data <- z$get()
+    i<- solve(data, ...)
+    z$setinv(i)
+    i
 }
